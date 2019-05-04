8004ac51135ada3417e17df60def69dcf005b13e
Denis Vitios <denvitios@gmail.com>
2019-05-04 16:54:32 +0300

    progect init, start working on arhitecture

--- DVVK/Classes/AppDelegate.swift
+++ DVVK/Classes/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = UIViewController()
         return true
     }
 
---
+++ DVVK/Classes/Routers/Router.swift
@@ -0,0 +1,9 @@
+//
+//  Router.swift
+//  DVVK
+//
+//  Created by Denis on 5/4/19.
+//  Copyright © 2019 Denis. All rights reserved.
+//
+
+import Foundation
